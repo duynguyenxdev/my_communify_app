e5dbc4d2c8f22ea1eabd98719eb56f22866b65a1
Nguyen Khanh Duy <khanhduyx2@gmail.com>
2026-06-11 17:50:18 +0700

    update

--- lib/core/models/exception.dart
+++ lib/core/models/exception.dart
@@ -1,6 +1,9 @@
 sealed class AppException implements Exception {
   final String message;
   AppException(this.message);
+
+  @override
+  String toString() => 'AppException(message = $message)';
 }
 
 class ApiException extends AppException {
--- lib/features/auth/screens/sign_in_screen.dart
+++ lib/features/auth/screens/sign_in_screen.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:my_communify/assets/assets.dart';
-import 'package:my_communify/core/models/exception.dart';
 import 'package:my_communify/core/widgets/button/button.dart';
 import 'package:my_communify/features/auth/providers/auth_provider.dart';
 
@@ -14,17 +13,15 @@ class SignInScreen extends ConsumerWidget {
     ref.listen(authProvider, (prev, next) {
       next.whenOrNull(
         error: (error, stackTrace) {
-          if (error is ApiException) {
-            showDialog(
-              context: context,
-              builder: (context) {
-                return AlertDialog.adaptive(
-                  title: Text('Error'),
-                  content: Text(error.toString()),
-                );
-              },
-            );
-          }
+          showDialog(
+            context: context,
+            builder: (context) {
+              return AlertDialog.adaptive(
+                title: Text('Error'),
+                content: Text(error.toString()),
+              );
+            },
+          );
         },
       );
     });
